374d9e9355daf93ad4dbb268de75bc36c3b7a24c
Heather Greenberg <hgreenberg1017@gmail.com>
2023-03-28 21:51:12 -0700

    add functions

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // Assignment Code
+var characterLength = 8;
+var choiceArr = [];
+
+var upper= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var special = "!@#$%^&*(){}[]+<>/,.|~?";
+var numerics = "1234567890";
+var pwLength = document.querySelector("#length");
+var upChar = document.querySelector("#upper");
+var lcChar = document.querySelector("#lower");
+var specChar = document.querySelector("#special");
+var numChar = document.querySelector("#numerics");
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -10,5 +23,37 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+
+}
+
+function getPrompts() {
+  characterLength = parseInt(prompt("How many characters would you like? (8-128 characters)"))
+
+
+if(isNaN(characterLength) || characterLength < 8 || characterLength)
+    alert("Character length has to be a number between 8-128. Please try again")
+}
+
+if (confirm("Would you like to use lowercase letters?")) {
+    choiceArr = choiceArr.concat(lowerCaseArr);
+}
+
+if (confirm("Would you like to use uppercase letters?")) {
+  choiceArr = choiceArr.concat(upperCaseArr);
+}
+
+if (confirm("Would you like to use special characters?")) {
+  choiceArr = choiceArr.concat(specialCharArr);
+}
+
+if (confirm("Would you like to use numbers?")) {
+  choiceArr = choiceArr.concat(numberArr);
+}
+
+return true;
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword > 128);
+
+
